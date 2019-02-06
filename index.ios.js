1dc667a98ea4f2e9143616180ba8a257bb3c5fa7
jcharbo <jcharbo@users.noreply.github.com>
2019-02-06 16:43:53 -0600

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,11 @@
-import React from 'react'
-import {NativeAppEventEmitter, NativeModules} from 'react-native'
+
+/* @flow */
+'use strict';
+
+import { NativeModules, StyleSheet, Image } from 'react-native';
+import React, { Component } from 'react';
 const {SilentSwitch} = NativeModules
 
-let eventListener = null
 
-export default {
-  addEventListener: callback => {
-    SilentSwitch.subscribe()
-    eventListener = NativeAppEventEmitter.addListener('SilentSwitch', ({status}) => {
-      if (status === 'ON') return callback(true)
-      if (status === 'OFF') return callback(false)
-    })
-  },
-  removeEventListener: () => eventListener.remove()
-}
+const SilentSwitch = NativeModules.SilentSwitch;
+export default SilentSwitch
